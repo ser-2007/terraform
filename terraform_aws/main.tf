97cdfd363c6244bf83d855c6317593e8d974a8dd
sersah95 <sersah95@gmail.com>
2022-06-13 00:50:50 +0300

    terraform aws structure main tf added

---
+++ terraform_aws/main.tf
@@ -0,0 +1,97 @@
+resource "aws_vpc" "ser_vpc" {
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "dev"
+  }
+}
+
+resource "aws_subnet" "ser_public_subnet" {
+  vpc_id                  = aws_vpc.ser_vpc.id
+  cidr_block              = "10.123.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "ser_internet_gateway" {
+  vpc_id = aws_vpc.ser_vpc.id
+  tags = {
+    "Name" = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "ser_public_rt" {
+  vpc_id = aws_vpc.ser_vpc.id
+
+  tags = {
+    "Name" = "dev_public_rt"
+  }
+  
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.ser_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.ser_internet_gateway.id
+}
+
+resource "aws_route_table_association" "ser_public_assoc" {
+  subnet_id = aws_subnet.ser_public_subnet.id
+  route_table_id = aws_route_table.ser_public_rt.id 
+}
+
+
+resource "aws_security_group" "ser_sg" {
+  name = "dev-sg"
+  description = "dev security group"
+  vpc_id = aws_vpc.ser_vpc.id
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_key_pair" "ser_auth" {
+  key_name = "s"
+  public_key = file("~/.ssh/s.pub") #your ssh-key path
+  
+}
+
+resource "aws_instance" "dev_node" {
+  instance_type = "t2.micro"
+  ami = data.aws_ami.server_ami.id  
+  key_name = aws_key_pair.ser_auth.id  
+  vpc_security_group_ids = [aws_security_group.ser_sg.id]
+  subnet_id = aws_subnet.ser_public_subnet.id  
+  user_data = file("userdata.tpl")
+  root_block_device {
+    volume_size = 10
+  }
+  tags = {
+    "Name" = "dev-node"
+  }
+
+  provisioner "local-exec" {
+    command = templatefile("${var.host_os}-ssh-config.tpl", {
+      hostname = self.public_ip,
+      user     = "ubuntu",
+    identityfile = "~/.ssh/s" })
+    interpreter = ["Powershell", "-Command"]
+  }
+}
