84a874e5886be01f82ac4ad702f0590d108c4bc0
sersah95 <sersah95@gmail.com>
2022-06-23 19:09:10 +0300

    github.tf file updated

--- terraform_beginning/github.tf
+++ terraform_beginning/github.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     github = {
-        source = "integrations/github"
-        version = "4.17.0"
+      source = "integrations/github"
+      version = "4.26.1"
     }
   }
 }
