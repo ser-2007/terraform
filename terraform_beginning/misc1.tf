207ab0424fbcd25d235d866dc4be5d8bebad0028
sersah95 <sersah95@gmail.com>
2022-06-26 20:47:04 +0300

    update

--- terraform_beginning/misc1.tf
+++ terraform_beginning/misc1.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAXZPBX2U3TAZ46EXS"
-  secret_key = "Wn4zZ8YVI32IvK+P8X34T+KEUi1entZG6QNzieYS"
+  access_key = "write your own key"
+  secret_key = "write your own key"
 }
 
 resource "aws_iam_user" "user" {
